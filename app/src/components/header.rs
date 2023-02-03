29d2980669b16856d2f53c52d1fc6a87715b710a
CrumblyLiquid <crumblyliquid@gmail.com>
2023-02-03 19:25:49 +0100

    Make header return to root route

--- app/src/components/header.rs
+++ app/src/components/header.rs
@@ -11,9 +11,11 @@ pub struct Props {
 pub fn header(props: &Props) -> Html {
     html! {
         // <header class="flex items-center justify-center shadow-sm text-lg space-x-4 p-1 bg-gray-50">
-        <header class="flex items-center justify-center text-lg space-x-4 p-1 bg-gray-100 border border-gray-100 border-b-gray-200">
-            <img class="w-10 h-10" src={props.icon_path.clone()}/>
-            <span>{props.name.clone()}</span>
+        <header class="">
+            <a href="/" class="flex items-center justify-center text-lg space-x-4 p-1 bg-gray-100 border border-gray-100 border-b-gray-200">
+                <img class="w-10 h-10" src={props.icon_path.clone()}/>
+                <span>{props.name.clone()}</span>
+            </a>
         </header>
     }
 }
